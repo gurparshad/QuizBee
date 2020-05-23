d04447a63acdf3eac8b4b8d94edde5e5bb3ccbf4
Gurparshad Sharma <39668145+gurparshad@users.noreply.github.com>
2020-05-24 00:22:02 +0300

    Add files via upload

---
+++ build/precache-manifest.046f7cfed62e5f3a4bcfb0671cd4f817.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8f3cbeebdcd9c010088750501c64c53f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6a06efca002bba155ad",
+    "url": "/static/css/main.0f93a89c.chunk.css"
+  },
+  {
+    "revision": "63c2316b85f3801116bb",
+    "url": "/static/js/2.5f3dc2ad.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5f3dc2ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6a06efca002bba155ad",
+    "url": "/static/js/main.2c725903.chunk.js"
+  },
+  {
+    "revision": "7023c3619a99971d8ff8",
+    "url": "/static/js/runtime-main.7eb5ae4b.js"
+  }
+]);
